ee7e64c7f635bae72297a43c4e832b7fda785c5d
mattia <mattia.fedrigo@hotmail.it>
2024-08-06 16:18:55 +0200

    config to run on server

--- src/models.py
+++ src/models.py
@@ -34,9 +34,9 @@ def get_embeddings() -> Embeddings:
             return OllamaEmbeddings(model="llama3")
         case 'ollama', 'nomic-embed-text':
             return OllamaEmbeddings(model="nomic-embed-text")
-        case 'vllm', 'nomic-embed-text':
+        case 'vllm', 'llama3.1':
             return OpenAIEmbeddings(
-                model='nomic-ai/nomic-embed-text-v1.5',
+                model='meta-llama/Meta-Llama-3.1-8B-Instruct',
                 openai_api_key='EMPTY',
                 openai_api_base=config.LLM_SERVER
             )
