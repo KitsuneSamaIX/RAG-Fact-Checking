ecdf566ef1af1ce69ded6f335823a7d7dcec1a22
mattia <mattia.fedrigo@hotmail.it>
2024-08-06 23:57:23 +0200

    Added metrics and better report overall

--- src/test_suite.py
+++ src/test_suite.py
@@ -15,8 +15,13 @@ def run_test_suite(df: pd.DataFrame):
     ids = df.index.to_series()
     unique_ids = ids.drop_duplicates()
 
+    # Initialize statistics
+    n_total = len(unique_ids)
     n_correct = 0
     n_error = 0
+    n_true_positive = 0
+    n_false_positive = 0
+    n_false_negative = 0
 
     for id in unique_ids:
         try:
@@ -39,16 +44,36 @@ def run_test_suite(df: pd.DataFrame):
             if res == target:
                 n_correct += 1
 
+            if res is True and target is True:
+                n_true_positive += 1
+
+            if res is True and target is False:
+                n_false_positive += 1
+
+            if res is False and target is True:
+                n_false_negative += 1
+
         except Exception as e:
             print(f"WARNING: an exception occurred while checking the ID {id}. {e}")
             n_error += 1
 
     # Report statistics
-    print(f"\n\n\n\nREPORT:")
-    print(f"Checked IDs: {len(unique_ids)}")
-    print(f"Correct answers: {n_correct} ({n_correct / len(unique_ids) * 100}%)")
-    print(f"ID checks aborted due to errors: {n_error}")
-    print(f"Accuracy (excluding errors): {n_correct / (len(unique_ids) - n_error) * 100}%")
+    print("\n\n\n\nREPORT:")
+    if n_total == n_error:
+        print("All ID checks have been aborted due to errors! Check code or network configuration.")
+    else:
+        print(f"Checked IDs: {n_total}")
+        print(f"Correct answers: {n_correct} ({(n_correct / n_total) * 100}%)")
+        print(f"ID checks aborted due to errors: {n_error}")
+        print("\nMETRICS (excluding aborted checks):")
+        accuracy = n_correct / (n_total - n_error)
+        precision = n_true_positive / (n_true_positive + n_false_positive)
+        recall = n_true_positive / (n_true_positive + n_false_negative)
+        f1 = 2 * ((precision * recall) / (precision + recall))
+        print(f"Accuracy: {accuracy}")
+        print(f"Precision: {precision}")
+        print(f"Recall: {recall}")
+        print(f"F1: {f1}")
 
 
 def _get_fact_and_target(id: str, df: pd.DataFrame) -> tuple[Fact, int]:
