a567d39340f38d716672407975e9f7b708d33cdf
mattia <mattia.fedrigo@hotmail.it>
2024-08-17 02:23:57 +0200

    config update

--- src/config.py
+++ src/config.py
@@ -250,7 +250,7 @@ class _UniudMitel3Server(_Common):
     ALL_EVIDENCE_VECTOR_STORE_PATH = '/mnt/dmif-nas/SMDC/datasets/Misinfo-Truncated-Rankings-RAG/data/cikm2024_soprano/embeddings/512'
     CACHED_URLS_PATH = '/mnt/dmif-nas/SMDC/datasets/Misinfo-Truncated-Rankings-RAG/data/cikm2024_soprano/evidence_to_index'
 
-    RETRIEVAL_MODE = 'bing+vs'
+    RETRIEVAL_MODE = 'vs'
     USE_RERANKER = True
 
     HUGGING_FACE_CACHE = '/mnt/dmif-nas/SMDC/HF-Cache'
