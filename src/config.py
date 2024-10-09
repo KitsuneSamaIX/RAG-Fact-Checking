5820844a280f189bffa6c7ca82f2c9398de9dfbc
mattia <mattia.fedrigo@hotmail.it>
2024-10-09 05:16:56 +0200

    server config

--- src/config.py
+++ src/config.py
@@ -326,7 +326,7 @@ class _UniudMitel3Server(_Common):
     DATASET_PATH_PREFIX = '/mnt/dmif-nas/SMDC/datasets/Misinfo-Truncated-Rankings-RAG/data/'
     DATASET_NAME = 'cikm2024_soprano'
 
-    GROUND_TRUTH_DATASET_PATH = os.path.join(DATASET_PATH_PREFIX, DATASET_NAME, 'ground_truth.pqt')
+    GROUND_TRUTH_DATASET_PATH = os.path.join(DATASET_PATH_PREFIX, DATASET_NAME, 'ground_truth.csv')
     # SEARCH_ENGINE_RESULTS_DATASET_PATH = os.path.join(DATASET_PATH_PREFIX, DATASET_NAME, 'df_evidence_list-top10.csv')
     ALL_EVIDENCE_VECTOR_STORE_PATH = os.path.join(DATASET_PATH_PREFIX, DATASET_NAME, 'embeddings/512/')
     ALL_EVIDENCE_VECTOR_STORE_INDEX_NAME = 'FAISS_INDEX_CHUNK_SIZE_512'
